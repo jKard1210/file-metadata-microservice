9136a1305b9b9ef3634235f05abbb4546ca90081
James Kardatzke <kardatzkejames@yahoo.com>
2017-02-28 22:39:55 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express()
+var multer  = require('multer');
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage })
+
+app.get("/", function(req,res){
+    res.sendfile(__dirname + "/page.html");
+})
+
+app.post("/api/readFile", upload.single("file"), function(req,res){
+    res.json({"size": req.file.size + " bytes"})
+})
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 3000!')
+})
+
